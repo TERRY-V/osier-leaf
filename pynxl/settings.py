c28a32d335d3d5cc469e54bac64044259cbf22db
TERRY-V <80720365@qq.com>
2016-10-13 14:41:52 +0800

    add staticfiles_dir param

--- pynxl/settings.py
+++ pynxl/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'l4jcu)-n&yz70sf=$=0x)226@_rgc0b4&nq#9)x^45bzyi@qov'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['liushaoer.com', 'www.liushaoer.com']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'image',
     'star',
     'health',
+    'shopping',
     'usercenter',
     'grappelli',
     'captcha',
@@ -131,16 +132,19 @@ USE_I18N = True
 
 USE_L10N = True
 
-#USE_TZ = True
 USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = BASE_DIR + '/static/'
-
 STATIC_URL = '/static/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # User model
 
